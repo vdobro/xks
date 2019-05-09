61caa17b610714e7b04f2ecb8526fb76cb2d1725
Vitalijus Dobrovolskis <vitalijusdobro@gmail.com>
2019-05-10 01:04:41 +0200

    xks-2 Added empty NUnit test projects.

---
+++ XKS.Data.Tests/SampleTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace XKS.Data.Tests
+{
+	public class SampleTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void DataTest()
+		{
+			Assert.Pass();
+		}
+	}
+}
---
+++ XKS.Domain.Tests/SampleTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace XKS.Domain.Tests
+{
+	public class SampleTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void DomainTest()
+		{
+			Assert.Pass();
+		}
+	}
+}
