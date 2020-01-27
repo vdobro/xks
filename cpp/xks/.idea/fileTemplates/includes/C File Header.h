7e0f16aa9ee2bb6e999dd317fb28446957e671dc
Vitalijus Dobrovolskis <vitalijusdobro@gmail.com>
2020-01-27 15:29:27 +0100

    xks-5 Added Windows build dependencies to README

---
+++ cpp/xks/.idea/fileTemplates/includes/C File Header.h
@@ -0,0 +1,9 @@
+#if ($HEADER_COMMENTS)
+//
+// Created by Vitalijus Dobrovolskis on ${DAY}.${MONTH}.${YEAR}
+#if ($ORGANIZATION_NAME && $ORGANIZATION_NAME != "")
+// Copyright (c) $YEAR ${ORGANIZATION_NAME}#if (!$ORGANIZATION_NAME.endsWith(".")).#end All rights reserved.
+#end
+//
+#end
+
--- cpp/xks/main.cpp
+++ cpp/xks/main.cpp
@@ -1,8 +1,12 @@
+//
+// Created by Vitalijus Dobrovolskis on 25.01.2020
+//
+
 #include "MainApplication.h"
 
 int main(int argc, char *argv[]) {
 	xks::MainApplication app(argc, argv);
 
 	app.run();
-    return 0;
+	return 0;
 }
