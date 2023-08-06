c42d86edab2504c0269de4d5080806da7a6a9877
Vitalijus Dobrovolskis <vitalijus@dobrovolskis.com>
2023-08-06 21:48:10 +0200

    added missing environment variable in production deployment script

--- deployment/production.sh
+++ deployment/production.sh
@@ -3,6 +3,7 @@ set -eou pipefail
 
 export APPLICATION_HOST="xks.dobrovolskis.com"
 export COUCHDB_USERNAME="xks"
+export INTERNAL_COUCHDB_URL="http://db:5984"
 
 # if not set, use the default test password
 if [ -z ${COUCHDB_PASSWORD+x} ]; then
@@ -12,4 +13,4 @@ fi
 docker pull couchdb:3.3
 docker pull docker.io/dobrovolskis/xks:0.1-SNAPSHOT
 
-docker compose -p xks -f compose-base.yaml -f compose-production.yaml up
+docker compose -p xks -f compose-base.yaml -f compose-production.yaml up -d
