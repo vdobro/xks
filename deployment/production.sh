9015ec5b28a37a050099d551f5e33379d1fd4d8f
Vitalijus Dobrovolskis <vitalijus@dobrovolskis.com>
2023-08-06 21:29:36 +0200

    added "latest" as Docker image tag to the release script

--- deployment/production.sh
+++ deployment/production.sh
@@ -9,4 +9,7 @@ if [ -z ${COUCHDB_PASSWORD+x} ]; then
     export COUCHDB_PASSWORD="Password1234"
 fi
 
+docker pull couchdb:3.3
+docker pull docker.io/dobrovolskis/xks:0.1-SNAPSHOT
+
 docker compose -p xks -f compose-base.yaml -f compose-development.yaml up
--- docker_release.sh
+++ docker_release.sh
@@ -8,3 +8,5 @@ mvn clean package -DskipTests
 
 docker build -t dobrovolskis/xks:0.1-SNAPSHOT .
 docker push dobrovolskis/xks:0.1-SNAPSHOT
+docker tag dobrovolskis/xks:0.1-SNAPSHOT dobrovolskis/xks:latest
+docker push dobrovolskis/xks:latest
